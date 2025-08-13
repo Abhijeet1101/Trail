897e79faba62141c57503793f30da563b14879bd
Abhijeet1101 <abhijeetra20@gmail.com>
2025-08-13 10:10:13 +0530

    2nd commit

--- Oops2.py
+++ Oops2.py
@@ -8,18 +8,23 @@ class Bank:
         user_input=input(''' 
         1. Press 1 for Pin Change
         2. Press 2 for Balance Enquiry
-        3. Press 3 for Withdrawl ''')
+        3. Press 3 for Withdrawl
+        4. Press 4 to Exit ''')
         if user_input=='1':
             self.pin_change()
         elif user_input=='2':
             self.bal_enquiry()
         elif user_input=='3':
             self.withdrwal()
+        elif user_input=='4':
+            exit()
+            
     def pin_change(self):
         new_pin=input("Enter Your New Pin :")
         self.pin=new_pin
         new_bal=int(input("Enter amount you want to deposite : "))
         self.bal=new_bal
+        self.Menu()
     def bal_enquiry(self):
         verify=input("Enter your pin :")
         if verify==self.pin:
@@ -27,6 +32,7 @@ class Bank:
         else:
             print("You entered a wrong pin.... Try Again")
             self.bal_enquiry()
+        self.Menu()
     def withdrwal(self):
         verify=input("Enter your pin :")
         if verify==self.pin:
@@ -36,6 +42,10 @@ class Bank:
                 print(f"You have withdrawn {amount} your reamaing balance is {self.bal}")
             else:
                 print("Insufficient Balance")
+        else:
+            print("You entered a wrong pin.... Try Again")
+            self.withdrwal()
+        self.Menu()
 
 
 c1=Bank()
